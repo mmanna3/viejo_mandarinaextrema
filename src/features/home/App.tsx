810348082c5c7cf8fbbadf8d6fb7943c6cad02a6
mmanna3 <mmmannna3@gmail.com>
2022-06-16 02:05:13 -0300

    Migración a CRA

---
+++ src/features/home/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import mandarina from '../../resources/images/mandarina.png';
+import spotify from '../../resources/images/spotify.png';
+import instagram from '../../resources/images/instagram.png';
+import youtube from '../../resources/images/youtube.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="contenedor-principal">
+    <img className="mandarina" src={mandarina} alt="mandarina"/>    
+    <div className="youtube-video-contenedor">
+      <iframe height="250" width="320" title="cancion-para-desayunar" 
+        src="https://www.youtube.com/embed/wAl7HHFWCIg">
+      </iframe>  
+    </div>
+    <div className="iconos-redes-contenedor">
+      <a href="https://instagram.com/mandarina.extrema">
+        <img className="icono-red" src={instagram} alt="instagram"/>
+      </a>
+      <a href="https://open.spotify.com/artist/7lo1iNrrVW77oauQUED0tF">
+        <img className="icono-red spotify" src={spotify} alt="spotify"/>
+      </a>
+      <a href="https://www.youtube.com/channel/UC1YAsQ6YbvE4lXEKux-U3Yg/featured">
+        <img className="icono-red" src={youtube} alt="youtube"/>
+      </a>
+    </div>      
+  </div>
+  );
+}
+
+export default App;
