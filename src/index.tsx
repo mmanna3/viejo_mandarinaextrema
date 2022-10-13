1a0eb1ac4c3bdb0314e27cb0892b776b9171c09f
mmanna3 <mmmannna3@gmail.com>
2022-10-13 02:21:41 -0300

    Para que anden las rutas cambia BrowserRouter por HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</BrowserRouter>
+		<HashRouter>
+			<App />
+		</HashRouter>
 	</React.StrictMode>
 );
 
