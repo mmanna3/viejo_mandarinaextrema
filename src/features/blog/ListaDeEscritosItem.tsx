9fa86646fd7e3de6b55496b4bea85b005348b864
mmanna3 <mmmannna3@gmail.com>
2022-10-15 04:04:32 -0300

    Estilo listaDeEscritosItem

--- src/features/blog/ListaDeEscritosItem.tsx
+++ src/features/blog/ListaDeEscritosItem.tsx
@@ -1,5 +1,4 @@
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
+import { Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { IEscrito } from "../../firebase";
 
@@ -10,17 +9,19 @@ interface IEscritoInput extends IEscrito {
 const Escrito = (escrito: IEscritoInput) => {
 	
 	const navigate = useNavigate();
-	const obtenerResumen = (texto: string) => texto.slice(0, 80) + (texto.length > 80 ? "..." : ""); 
+	const obtenerResumen = (texto: string) => texto.slice(0, 20) + (texto.length > 20 ? "..." : ""); 
 
-	return <>
-		<ListItem alignItems="flex-start">
-			<ListItemText
-				primary={escrito.titulo}
-				secondary={obtenerResumen(escrito.cuerpo)}
-				onClick={() => navigate(escrito.id)}
-			/>
-		</ListItem>
-	</>;
+	return <Grid padding="0.8rem 1rem" onClick={() => navigate(escrito.id)}>
+		<Typography variant="body2" style={{fontFamily: "monospace", color: "#333", fontSize: "0.7rem"}} component="div">
+			{escrito.fechaHora.substring(0, 10)}
+		</Typography>
+		<Typography variant="body1" style={{fontFamily: "monospace", fontSize: "1rem"}} component="div">
+			{escrito.titulo}
+		</Typography>
+		<Typography variant="body2" style={{fontFamily: "monospace", color: "#666", fontSize: "0.9rem"}} component="div">
+			{obtenerResumen(escrito.cuerpo)}
+		</Typography>
+	</Grid>;
 };
 
 export default Escrito;
